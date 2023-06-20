e066439983cc7158f430b255db03a369ac468b22
ihor97 <daniel.waffner@gmail.com>
2023-06-20 21:52:25 +0100

    brewery

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-// статичні методи,поля - відносяться до класу в цілому а не до інстанса
 
-function Car(model) {
-    this.model=model
-    Car.amount++
+class Brewery {
+    constructor(id, name, website) {
+        this.id = id
+        this.name = name
+        this.website = website
+    }
+
+    renderIn(elem) {
+        elem.insertAdjacentHTML('beforeend',
+            `
+       <div class="brewery">
+        <h3>${this.name} [${this.id}]</h3>
+       <a href="${this.website}">${this.website} </a>
+
+        </div>
+       `
+        )
+    }
 }
-Car.amount=0
-Car.printAmount=function () {
-    console.log(this.amount);
+
+class App {
+    constructor(elem) {
+        this.elem = elem
+        this.init()
+
+
+
+    }
+
+    async init() {
+        const data = await this.loadData()
+        const breweries = data.map(item => {
+            return new Brewery(item.id, item.website_url, item.name)
+        })
+        breweries.forEach(item => {
+            item.renderIn(this.elem)
+        });
+
+
+    }
+
+    async loadData() {
+        return (await axios.get('https://api.openbrewerydb.org/breweries')).data
+    }
 }
+
+
+new App(document.getElementById('app'))
