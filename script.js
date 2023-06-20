e0eff36891761ef01ca5b33b082f38a6b50d6107
ihor97 <daniel.waffner@gmail.com>
2023-06-20 19:23:24 +0100

    приклад на прототипи

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
-// для створення класів є три патерна в js - фабрика конструктор і прототип
-// фабрика
-// function Human(name,age) {
-//     return {
-//         name,
-//         age,
-//         getBirthYear(){
-//             return (new Date()).getFullYear()-this.age
-//         }
-//     }
-// }
+// в обєкта є властивість конструтор яка вказує на функцію коснтруктор
 
-// конструктор 
-// function Human(name,age) {
-//         this.name=name
-//         this.age=age
-//         // погано тим що всеодно ф-я буде в обєкті але вде можемо заюзати instanceof
-// дозволяє зробити наслідування але створює дубоікати методів
-//         this.getBirthYear=function(){
-//             return (new Date()).getFullYear()-this.age
-//         }
-// }
+function Student(name,course,marks) {
+    this.name=name
+    this.course=course
+    this.marks=Array.isArray(marks)? marks:[]
+}
 
-// прототип
-// function Human(name, age) {
-//     this.name = name
-//     this.age = age
+Student.prototype.getAverageMark=function(){
+    return this.marks.reduce((s,v)=>s+v,0)/this.marks.length
+}
+// ще добавили гетер щоб просто була властивість 
+Object.defineProperty(Student.prototype,'averageMark',
+{
+    get(){
+        return this.getAverageMark()
+    }
+})
 
-// }
-// Human.prototype.getBirthYear = function () {
-//     return (new Date()).getFullYear() - this.age
-// }
+const student=new Student('Alex',3,[3,4,2,5,1,5,2])
+console.log(student);
+console.log(student.averageMark);
+
+
+function Faculty(name) {
+    this.name=name
+    this.students=[]
+}
+
+Faculty.prototype.addStudent=function (student) {
+    this.students.push(student)
+}
+Faculty.prototype.getAverageMark=function () {
+    return this.students.reduce((sum,student)=>sum+student.averageMark,0)/this.students.length
+}
+const faculty=new Faculty('web')
+faculty.addStudent(new Student('alex',5,[3,4,5,1,5,3,5]))
+faculty.addStudent(new Student('ivan',3,[2,42,2,4,15]))
+faculty.addStudent(new Student('maria',5,[3,4,5,1,5,3,5]))
+faculty.addStudent(new Student('marta',1,[3,4,5,1,323,3,5]))
+faculty.addStudent(new Student('oleg',5,[3,4,5,1,5,3,5]))
+
+console.log(faculty.getAverageMark());
